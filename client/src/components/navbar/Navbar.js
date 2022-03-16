3c401bfc7377337e663d7eaa53f16ef4a9986677
arif <abcvpc@gmail.com>
2022-03-16 22:50:40 +0600

    static navbar no function

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -4,26 +4,47 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <div className="navbar">
-      <nav>
-        <div className="left">
+      <nav className="navbar-upper">
+        <div className="navbar-left">
           <Link to="/">
             <h1>HomeLogo</h1>
           </Link>
         </div>
         {/* <SearchBar></SearchBar> */}
-        <div className="center">
+        <div className="navbar-center">
           <label>
-            <input className="search-input" placeholder="search" type="text" />
+            <input
+              className="navbar-center-search-input"
+              placeholder="search"
+              type="text"
+            />
           </label>
         </div>
-        <div className="right">
-          <div className="calls">
+        <div className="navbar-right">
+          <div className="navbar-right-calls">
             <h1>available</h1>
             <p>call now</p>
           </div>
           <i>icon</i>
           <i>icon</i>
           <i>icon</i>
+        </div>
+      </nav>
+
+      <nav className="navbar-bottom">
+        <div className="navbar-bottom-names">
+          <ul>
+            <li>NEW IN </li>
+            <li>MUST HAVE </li>
+            <li>TREND </li>
+            <li>THEME DEMOS </li>
+            <li>CHILD THEME</li>
+            <li>PAGES</li>
+            <li>BUY THEME</li>
+          </ul>
+        </div>
+        <div className="navbar-bottom-support">
+          <h1>help | lang</h1>
         </div>
       </nav>
     </div>
