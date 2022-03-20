783fbdd5c5eabfb63ce873673c8c70ec503666e5
rahmanShuhin <suhin4000@gmail.com>
2022-03-20 12:45:42 +0600

    backend merged middlewares folder

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
-
-const express = require("express")
-const bd = require('./database/index')
-const middleware = require('./middlewares/index')
-const userRouter = require('./routers/userRoute')
-
-const app = express()
-
+const express = require("express");
+const bd = require("./database/index");
+const middleware = require("./middlewares/index");
+const userRouter = require("./routers/userRoute");
+const errorMiddleWare = require("./middlewares/error");
+const app = express();
 
 require("dotenv").config();
 
 //BD CONNECTION
 bd.connectToAtlas();
 
-
 //middleware
 app.use(...middleware);
 
 //router connection
-app.use(userRouter)
-
-
-
+app.use(userRouter);
 
 //server connection
 const PORT = process.env.PORT || 4000;
@@ -30,26 +24,3 @@ app.listen(PORT, () => {
 
 //middleware for error
 app.use(errorMiddleWare);
-
-// //routing import
-// const userRouter = require("./routers/userRoute");
-// //middleware
-
-// //Routes
-// app.use("api/users/", userRouter);
-
-// app.get("/", (req, res) => {
-//   res.json({
-//     message: "Welcome",
-//   });
-// });
-
-// //database connection
-// // mongoose.connect(process.env.DATABASE__CONNECTION, () => {
-// //   console.log("database connect");
-// // });
-
-// const PORT = process.env.PORT || 4000;
-// app.listen(PORT, () => {
-//   console.log(`server is running on ${PORT}`);
-// });
