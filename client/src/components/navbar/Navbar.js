9a5e9322610d8d8830380b944f970cea742c2f5b
arif <abcvpc@gmail.com>
2022-03-22 22:05:56 +0600

    skeleton of navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -2,23 +2,33 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className="header">
-      <div className="header--wrapper">
-        <article className="header--wrapper--contact">
-          <div>
-            i <span>+88012 3456 7894</span>{" "}
-          </div>
-          <div>
-            i <span>aldflasfj@gmail.com</span>{" "}
-          </div>
-        </article>
-        <article className="header--wrapper--help">
-          <div>Faq</div>
-          <div>need help</div>
-          <div>lang</div>
-          <div>currency</div>
-        </article>
-      </div>
-    </nav>
+    <>
+      <header className="header">
+        <div className="header--wrapper">
+          <article className="header--wrapper--contact">
+            <div>
+              📞<span> +88012 3456 7894</span>{" "}
+            </div>
+            <div>
+              📧<span> aldflasfj@gmail.com</span>{" "}
+            </div>
+          </article>
+          <article className="header--wrapper--help">
+            <div>Faq</div>
+            <div>need help</div>
+            <div>lang</div>
+            <div>currency</div>
+          </article>
+        </div>
+      </header>
+
+      <nav className="navbar">
+        <section className="navbar--wrapper">
+          <article>logo</article>
+          <article>search</article>
+          <article>icon</article>
+        </section>
+      </nav>
+    </>
   );
 }
