d3e3ff2bc36b6402cbbbfbf8ea54527a5065f3c0
Shah Jahidul Islam Riad <shahriad555@gmail.com>
2022-03-26 18:14:28 +0600

    cart update

--- client/src/components/SideCart/SideBarCart.js
+++ client/src/components/SideCart/SideBarCart.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useOnClickOutside } from '../../hooks/useOnClickOutside'
+import BagIcon from '../IconComponents/BagIcon'
 
 const SideBarCart = ({openSideBarCart, setOpenSideBarCart}) => {
 
@@ -8,11 +9,23 @@ const SideBarCart = ({openSideBarCart, setOpenSideBarCart}) => {
 
   return (
       <>
-        
-          <div ref={cartRef} className={openSideBarCart ? 'sidebar--cart--main-wrapper': 'sidebar--cart--out'}>
-            SideCart
+        <div 
+          ref={cartRef} 
+          className={openSideBarCart ? 'sidebar--cart': 'sidebar--cart--out'}
+        >
+          <div className="sidebar--cart--wrapper">
+            <div className="cart--header">
+              <BagIcon/> <h3> 03 items</h3> 
+            </div>
+            <div className="cart--item">
+              <div className="add--to--cart">
+                <span className="cart--icon--wrapper">-</span>
+                <span className="counter-text">1</span>
+                <span className="cart--icon--wrapper">+</span>
+              </div>
+            </div>
           </div>
-        
+        </div>
         <div className={openSideBarCart && 'sidebar--cart--backdrop'}/>
       </>
     
