ef7631840d54887042c042831389e227cad6d649
Shah Jahidul Islam Riad <shahriad555@gmail.com>
2022-03-26 20:33:34 +0600

    sideBar cart added

--- client/src/components/SideCart/SideBarCart.js
+++ client/src/components/SideCart/SideBarCart.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { useOnClickOutside } from '../../hooks/useOnClickOutside'
-import BagIcon from '../IconComponents/BagIcon'
+import React from 'react';
+import { CART_DATA } from '../../assets/data/cartData';
+import { useOnClickOutside } from '../../hooks/useOnClickOutside';
+import BagIcon from '../IconComponents/BagIcon';
+import CrossIcon from '../IconComponents/CrossIcon';
 
 const SideBarCart = ({openSideBarCart, setOpenSideBarCart}) => {
 
@@ -17,16 +19,38 @@ const SideBarCart = ({openSideBarCart, setOpenSideBarCart}) => {
             <div className="cart--header">
               <BagIcon/> <h3> 03 items</h3> 
             </div>
-            <div className="cart--item">
-              <div className="add--to--cart">
-                <span className="cart--icon--wrapper">-</span>
-                <span className="counter-text">1</span>
-                <span className="cart--icon--wrapper">+</span>
-              </div>
-            </div>
+            <div className="cart--item--wrapper">
+            {
+              CART_DATA.map((cart,i)=>(
+                <div className="cart--item">
+                  <div className="add--to--cart">
+                    <span className="cart--icon--wrapper">-</span>
+                    <span className="counter-text">1</span>
+                    <span className="cart--icon--wrapper">+</span>
+                  </div>
+                  <div className="cart--item--inside">
+                    <div className="cart--item--inside--img">
+                      <img src={require('../../assets/images/flash-1.webp')} width="100%" alt="" />
+                    </div>
+                    <div className="cart--item--inside--info">
+                      <h5>volvo car</h5>
+                      <p>$250 x 1</p>
+                      <p className='price'>$210.00</p>
+                    </div>
+                  </div>
+                  <div className='cross-icon'>
+                    <CrossIcon/>
+                  </div>
+                </div>
+              ))
+            }
+          </div>  
+          </div>
+          <div className='sidebar--cart--checkout'>
+            <button>Checkout</button>
           </div>
         </div>
-        <div className={openSideBarCart && 'sidebar--cart--backdrop'}/>
+        {openSideBarCart && <div className= 'sidebar--cart--backdrop'/>}
       </>
     
   )
