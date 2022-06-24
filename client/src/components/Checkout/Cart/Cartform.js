72fb46b9c6ba1f20263d6ba69fc493b1182e8962
arif <abcvpc@gmail.com>
2022-06-25 01:07:14 +0600

    added voucher jsx and css

--- client/src/components/Checkout/Cart/Cartform.js
+++ client/src/components/Checkout/Cart/Cartform.js
@@ -6,6 +6,7 @@ export default function Cartform() {
           <span className="cartform--header--text">Total:</span>
           <span className="cartform--header--total">$750.00</span>
         </div>
+        {/* additional comment */}
         <div className="cartform--divider"></div>
         <label>
           <span className="cartform--comments">
@@ -17,6 +18,18 @@ export default function Cartform() {
           <div className="cartform--textarea">
             <textarea name="" id="" cols="40" rows="10"></textarea>
           </div>
+        </label>
+        {/* voucher */}
+        <div className="cartform--divider"></div>
+        <label className="cartform--voucher">
+          <input
+            type="text"
+            className="cartform--voucher--input"
+            placeholder="voucher"
+          />
+          <button className="cartform--voucher--button">
+            <p>APPLY VOUCHER</p>
+          </button>
         </label>
       </form>
     </main>
