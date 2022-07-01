db9eb6469d5986568bfbc48a95c9c4a9c7c8d751
Shah Jahidul Islam Riad <shahriad555@gmail.com>
2022-07-02 02:14:37 +0600

    search bar updated

--- client/src/components/navbar/MainNavbar/NavbarSearch/NavbarSearch.js
+++ client/src/components/navbar/MainNavbar/NavbarSearch/NavbarSearch.js
@@ -1,18 +1,6 @@
-import { useRef, useState } from 'react';
-import { categories } from "../../../../assets/data/navdata";
-import DownIcon from "../../../IconComponents/DownIcon";
 import SearchIcon from "../../../IconComponents/SearchIcon";
-import { useOnClickOutsideWithSecondRef } from './../../../../hooks/useOnClickOutsideWithSecondRef';
 
 const NavbarSearch = () => {
-
-    const [showAllCategories, setShowAllCategories] = useState(false);
-
-    const categoryRef = useRef(null);
-    const categoryTextRef = useRef(null)
-
-    useOnClickOutsideWithSecondRef(categoryRef,categoryTextRef,
-      ()=> setShowAllCategories(false));
 
   return (
         <div className="navbar--wrapper--search">
@@ -23,24 +11,6 @@ const NavbarSearch = () => {
               <div className="navbar--wrapper--search--container--searchbox">
                 <input type="search" placeholder="search and hit enter.." />
               </div>
-              <div className="navbar--wrapper--search--container--dropdown">
-    
-                <div
-                    ref={categoryTextRef} 
-                    onClick={() => setShowAllCategories(!showAllCategories)}
-                >
-                    <h4>All categories</h4>
-                    <span className={showAllCategories ? "dropdown--icon rotate" : "dropdown--icon"}>
-                        <DownIcon />
-                    </span>
-                </div>
-                    <ul ref={categoryRef} className={showAllCategories ? "dropdown-links" : " "}>
-                    {showAllCategories &&
-                        categories.map((category) => (
-                        <li key={category.index}>{category.text}</li>
-                        ))}
-                    </ul>
-                </div>
             </div>
         </div>
   )
